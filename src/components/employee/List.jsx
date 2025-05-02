213909045dc877264c488cd2f3ba59a26129ed46
Mayank <mayank.semwalji@gmail.com>
2025-05-02 23:18:47 +0530

    Add login page UI

--- src/components/employee/List.jsx
+++ src/components/employee/List.jsx
@@ -34,6 +34,7 @@ const List = () => {
                 }
               ));
               setEmployees(data);
+              
               setFilteredEmployees(data)
             }
           } catch (error) {
