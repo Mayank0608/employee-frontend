5bba922513dad7c077dea8d7c2117bf0c2169b67
Mayank Semwal <145260981+Mayank0608@users.noreply.github.com>
2025-05-02 21:54:17 +0530

    Update Detail.jsx

--- src/components/leave/Detail.jsx
+++ src/components/leave/Detail.jsx
@@ -65,7 +65,7 @@ const Detail = () => {
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                     <div>
                         <img
-                            src={`https://employee-api-hkw1.onrender.com/public/uploads/${leave.employeeId.userId.profileImage}`}
+                            src={`https://employee-api-hkw1.onrender.com/${leave.employeeId.userId.profileImage}`}
                             className="rounded-full border w-72"
                         />
                     </div>
